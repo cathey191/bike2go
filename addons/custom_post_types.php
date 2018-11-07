7bdeba0493d25f4d3a2a22d004f28bb516713568
catherine <cathey191@hotmail.com>
2018-11-08 08:46:38 +1300

    Hiding Posts and Media from dashboard. Categories added to Products.

--- addons/custom_post_types.php
+++ addons/custom_post_types.php
@@ -3,7 +3,7 @@
   function add_bikes_post_type() {
     $labels = array(
       'name' => _x('Products', 'post type name', 'bike2gocustomtheme'),
-      'singular_name' => _x('Products', 'post type name', 'bike2gocustomtheme'),
+      'singular_name' => _x('Product', 'post type name', 'bike2gocustomtheme'),
       'add_new' => _x('Add New Product', 'adding new product', 'bike2gocustomtheme'),
       'add_new_item' => _x('Add New Product', 'adding new product', 'bike2gocustomtheme')
     );
@@ -15,10 +15,21 @@
       'hierarchical' => true,
       'menu_position' => 2,
       'menu_icon' => 'dashicons-screenoptions',
-      'supports' => array( 'title', 'comments', 'author', 'thumbnail')
+      'supports' => array( 'title', 'comments', 'author', 'thumbnail'),
+      'taxonomies' => array( 'category' )
     );
 
     register_post_type('products', $args);
   }
 
   add_action('init', 'add_bikes_post_type');
+
+  function hide_posts_types() {
+    ?>
+      <style type="text/css" media="screen">
+        #menu-posts, #menu-media {display:none;}
+      </style>
+    <?php
+  }
+
+  add_action('admin_head', 'hide_posts_types');
