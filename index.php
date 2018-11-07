6007236dfb60a83ae79ea892f24fd017c118ccce
catherine <cathey191@hotmail.com>
2018-11-08 09:19:47 +1300

    Home page only showing Products

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
     <h2>Catourage</h2>
     <div class="row">
       <div class="col">
-        <?php if(have_posts()): ?>
-          <div class="card-columns">
-            <?php while(have_posts()): the_post();?>
-              <?php get_template_part('content', get_post_format()); ?>
-            <?php endwhile; ?>
-          </div>
-        <?php endif; ?>
+        <div class="card-columns">
+          <?php
+            $args = array( 'post_type' => 'products');
+            $loop = new WP_Query( $args );
+            while ( $loop->have_posts() ) : $loop->the_post();
+          ?>
+            <?php get_template_part('content', get_post_format()); ?>
+          <?php endwhile; ?>
+        </div>
       </div>
     </div>
   </div>
